e092f263de692e548e7732aa7b3f77b0b9824fd7
Mayntu <daniilkhusnullin@gmail.com>
2025-01-28 11:53:58 +0500

    addings

--- src/controllers/club.py
+++ src/controllers/club.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from src.dto.club import ClubBase, CreateClubRequest
-from src.services.club import create_club, find_club
+from src.dto.club import ClubBase, CreateClubRequest, UpdateClubRequest
+from src.services.club import create_club, find_club, update_club, delete_club
 
 views : APIRouter = APIRouter()
 
@@ -14,3 +14,12 @@ async def post_club(request : CreateClubRequest) -> ClubBase:
 @views.get("/{club_id}", response_model=ClubBase, status_code=200)
 async def get_club(club_id : str) -> ClubBase:
     return await find_club(club_id=club_id)
+
+@views.patch("/{club_id}", response_model=ClubBase, status_code=200)
+async def patch_club(club_id : str, request : UpdateClubRequest) -> ClubBase:
+    return await update_club(club_id=club_id, request=request)
+
+@views.delete("/{club_id}", status_code=204)
+async def delete_club(club_id : str) -> None:
+    await delete_club(club_id=club_id)
+    return None
